d3483c89fdf9437c28c2664009fde67ebf0f0b07
Son Tran <tbson87@gmail.com>
2023-04-13 15:16:18 +0700

    WIP connect DB.

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,24 +1,46 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Query\Expression;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create("users", function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->unsignedBigInteger("workspace_id");
+            $table->integer("profile_type");
+            $table->string("name");
+            $table->string("email");
+            $table->string("mobile");
+            $table->string("password");
+            $table->boolean("is_owner")->default(false);
+            $table->json("group_ids")->default(new Expression("(JSON_ARRAY())"));
+
+            $table->string("token_signature");
+            $table->dateTime("token_refresh_expired");
+
+            $table->dateTime("email_verified_at")->nullable();
+            $table->dateTime("mobile_verified_at")->nullable();
+
             $table->rememberToken();
+
+            $table->dateTime("last_login_at")->nullable();
+            $table->dateTime("last_change_pwd")->nullable();
+
             $table->timestamps();
+
+            $table
+                ->foreign("workspace_id")
+                ->references("id")
+                ->on("workspaces");
+            $table->unique(["workspace_id", "email"]);
+            $table->unique(["workspace_id", "mobile"]);
         });
     }
 
@@ -27,6 +49,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists("users");
     }
 };
