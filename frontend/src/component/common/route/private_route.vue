402477b344af06442781eab81e13a259522a956f
Son Tran <tbson87@gmail.com>
2023-05-10 12:43:03 +0700

    Implement private route.

--- frontend/src/component/common/route/private_route.vue
+++ frontend/src/component/common/route/private_route.vue
@@ -1,4 +1,19 @@
-<script setup></script>
+<script setup>
+import { onMounted } from "vue";
+import { useRouter } from "vue-router";
+
+import StorageUtil from "util/storage_util";
+import NavUtil from "util/nav_util";
+
+const router = useRouter();
+
+onMounted(() => {
+    const authUser = StorageUtil.getAuthUser();
+    if (!authUser) {
+        NavUtil.navigateTo(router)("/login");
+    }
+});
+</script>
 
 <template>
     <div>
